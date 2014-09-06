5bbe6b1739f1d79a7ed2f9a97126e7005790bb44
edu-admin <edu-admin@edu-kiosk-000.(none)>
2014-09-07 00:36:47 +0300

    Google Drive launcher creation implemented

--- main.sh
+++ main.sh
@@ -6,7 +6,7 @@
 
 # Fixme: Add random sleep here? Or maybe I should be run 10 mins after every boot?
 
-sleep 15s;
+sleep 1s;
 date > /tmp/date-check.txt
 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games
 ansible-playbook -i /home/edu-admin/Github/elementary-os-kiosk-description/hosts /home/edu-admin/Github/elementary-os-kiosk-description/edu-kiosk-description.yml
