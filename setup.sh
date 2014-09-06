3a5122d6aff7c02130a262c4d5cc42da38cf4a84
edu-admin <edu-admin@edu-kiosk-000.(none)>
2014-09-07 01:40:36 +0300

    root check added

--- setup.sh
+++ setup.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
-sudo apt-add-repository ppa:ansible/ansible
-sudo apt-get update
-sudo apt-get -y install git ansible
-mkdir /home/edu-admin/Github
-cd /home/edu-admin/Github
-git clone https://github.com/erkkimon/elementary-os-kiosk-description.git
+
+if [ "$(id -u)" = "0" ]; then
+    echo "Please do not run this as root."
+    exit 1
+fi
+
+sudo apt-add-repository ppa:ansible/ansible &&
+sudo apt-get update &&
+sudo apt-get -y install git ansible &&
+mkdir /home/edu-admin/Github &&
+cd /home/edu-admin/Github &&
+git clone https://github.com/erkkimon/elementary-os-kiosk-description.git &&
 sudo bash /home/edu-admin/Github/elementary-os-kiosk-description/main.sh
