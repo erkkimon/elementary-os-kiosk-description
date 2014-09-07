55bc2d9d0ddf61905647c1764623b84009c8429f
Tom Himanen <tom.himanen@gmail.com>
2014-09-07 12:35:03 +0300

    Comments to main.sh

--- main.sh
+++ main.sh
@@ -4,12 +4,13 @@
 # (1) Ansible and Git must be installed and 
 # (2) elementary-os-kiosk-description repository must be cloned in /home/edu-admin/Github/
 
-# Fixme: Add random sleep here? Or maybe I should be run 10 mins after every boot?
+# Make sure that Ansible has the correct value in $PATH variable
+PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games &&
 
-sleep 1s;
-date > /tmp/date-check.txt
-PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games
+# Update the kiosk description and other relevant files
 rm -rf /home/edu-admin/Github/elementary-os-kiosk-description
 cd /home/edu-admin/Github
 git clone https://github.com/erkkimon/elementary-os-kiosk-description.git
+
+# Modify the kiosk according to the description using Ansible
 ansible-playbook -i /home/edu-admin/Github/elementary-os-kiosk-description/hosts /home/edu-admin/Github/elementary-os-kiosk-description/edu-kiosk-description.yml
