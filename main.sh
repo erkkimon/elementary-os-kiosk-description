fffd51df076ebafa273287143e6add1a0b3478b2
Tom Himanen <tom.himanen@gmail.com>
2014-09-07 12:07:49 +0300

    Repo copy removed before new one

--- main.sh
+++ main.sh
@@ -9,6 +9,7 @@
 sleep 1s;
 date > /tmp/date-check.txt
 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games
+rm -rf /home/edu-admin/Github/elementary-os-kiosk-description
 wget https://github.com/erkkimon/elementary-os-kiosk-description/archive/master.zip
 cd /home/edu-admin/Github/
 unzip elementary-os-kiosk-description-master.zip
