1a5245f85d5b3249e2af61539e3f5af5134a1936
edu-admin <edu-admin@edu-kiosk-000.(none)>
2014-09-07 11:42:02 +0300

    Fetch the latest config using 'git pull'

--- main.sh
+++ main.sh
@@ -9,4 +9,6 @@
 sleep 1s;
 date > /tmp/date-check.txt
 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games
+cd /home/edu-admin/Github/elementary-os-kiosk-description/
+git pull
 ansible-playbook -i /home/edu-admin/Github/elementary-os-kiosk-description/hosts /home/edu-admin/Github/elementary-os-kiosk-description/edu-kiosk-description.yml
